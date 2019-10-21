cbf91a2020768d130b09483878fe942eb3c69350
parjun8840 <54829402+parjun8840@users.noreply.github.com>
2019-10-21 21:13:58 +0530

    Update Product.java

--- src/parjun8840/product/entity/Product.java
+++ src/parjun8840/product/entity/Product.java
@@ -7,6 +7,7 @@ private double price;
 //Constructors are involved when objects are created. Compiler creates default constructor & it will have the same name
 //as that of of class & doesn't return type. If we write our own constructor then compiler doesn't creates one for us.
 //Always a constructor is require to create an object.
+// Generalization and specialization: IS-A relationship done using inheritance.
 public Product() { // Right click -> source -> generate constructor using fields.
 }
 public Product(int id, String name, double price) {
